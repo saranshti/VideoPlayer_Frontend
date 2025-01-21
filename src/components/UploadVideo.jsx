5826ce9f2bcf341840883daa1aa19c641a9bbd8c
saranshti <saranshvinodchandratiwari21000@gmail.com>
2025-01-22 04:15:05 +0530

    log Removed

--- src/components/UploadVideo.jsx
+++ src/components/UploadVideo.jsx
@@ -35,13 +35,11 @@ const UploadVideo = () => {
     }
     handleOpenModal();
     const date = Date.now();
-    console.log("first");
     const durationTime = await getVideoDurationFromVideoFile(file);
     const duration = convertSecondToMinuteandHour(durationTime);
     const chunkSize = 10 * 1024 * 1024; // 10MB per chunk
     const totalChunks = Math.ceil(file.size / chunkSize);
     const originalFileName = date + file.name;
-    console.log("originalFileName", originalFileName);
 
     setUploading(true);
     setProgress(0);
